8401cb14d50f36230ca9d24837c382ded360bafa
guimo <guimo@GUILLE-PC>
2018-09-21 17:20:49 +0200

    Fist commit

---
+++ Java/src/ITExercises/PowerSum.java
@@ -0,0 +1,36 @@
+package ITExercises;
+
+import java.util.Scanner;
+
+public class PowerSum {
+	
+	public static void main(String[] args) {
+		int base;
+		int exp;
+		int sum = 0;
+		int baseActual;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter base: ");
+		base = Integer.parseInt(scan.nextLine());
+		System.out.println("Please enter exponent: ");
+		exp = Integer.parseInt(scan.nextLine());
+		
+		baseActual = base;
+		
+		for(int i=0; i<(exp-1); i++) {
+			sum = 0;
+			for(int j=0; j<base; j++) {
+				sum += baseActual;
+			}
+			System.out.println("Iteration " +(i+1) +" gave a sum equal to " +sum);
+			baseActual = sum;
+		}
+		
+		System.out.println(base +" to the power of " 
+		+exp +" is equal to " +baseActual);
+		scan.close();
+	}
+ 
+}
